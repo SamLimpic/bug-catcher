9fc8bd1ff5eff85acad9acffeca35d3860a8e5ea
SamLimpic <samlimpic@gmail.com>
2021-05-03 13:55:54 -0600

    updated

--- bug-catcher.client/src/components/BugComponent.vue
+++ bug-catcher.client/src/components/BugComponent.vue
@@ -7,7 +7,7 @@
       <div class="col-md-3 col-8">
         <h3>{{ bugProp.title }}</h3>
       </div>
-      <div class="col-md-1 col-2 text-right px-0">
+      <div class="col-md-1 col-2 text-md-right text-left px-0">
         <img class="img-icon rounded-circle mx-auto" :src="bugProp.creator.picture" alt="">
       </div>
       <div class="col-3 d-md-block d-none">
@@ -23,13 +23,9 @@
           Caught
         </h3>
       </div>
-      <div class="col-2 d-md-none d-block text-left">
-        <h3 class="text-danger" v-if="bugProp.closed">
-          <i class="fas fa-bug text-danger mx-1" title="Released"></i>
-        </h3>
-        <h3 class="text-success" v-else>
-          <i class="fas fa-bug text-success mx-1" title="Caught"></i>
-        </h3>
+      <div class="col-2 d-md-none d-block text-md-left px-0 my-auto">
+        <i class="fas fa-bug text-danger mx-1" title="Released" v-if="bugProp.closed"></i>
+        <i class="fas fa-bug text-success mx-1" title="Caught" v-else></i>
       </div>
       <div class="col-2 d-md-block d-none">
         <h3>{{ `${bugProp.updatedAt.slice(5, 7)}/${bugProp.updatedAt.slice(8, 10)}/${bugProp.updatedAt.slice(0, 4)}` }}</h3>
